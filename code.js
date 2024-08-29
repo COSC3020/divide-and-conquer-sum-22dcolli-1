26c13664405355023243c013adca8a6ea1c1086e
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-08-29 10:25:45 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,21 @@
 //updated off of old repository divide-and-conquer-sum-22dcolli, specifically tried to limit non repeating variables.
 //i.e range = (high-low) because I noticed it was used more than once
-//as before credit to replit AI IDE and reminder of mergesort from GeeksforGeeks found in the original.
+//as before credit to replit AI IDE and a reminder example of mergesort from GeeksforGeeks found in the original and reinserted below.
+//I also need to mention while working through this I renamed the mergesort starting example to devideAndConquerSum so I could test it(I didn't have any reason to, but I hadn't thought about it)
+/*
+function devideAndConquerSum(a){
+    // if array is empty    
+    if(a.length==0)
+        return 0;
+    // if array has only 1 element
+    if(a.length==1)
+        return a[0];
+    // find mid of array
+    var mid = Math.floor(a.length/2);
+    // reccursively compute sum
+    return devideAndConquerSum(a.slice(0,mid))+devideAndConquerSum(a.slice(mid));
+}
+*/
 function divideAndConquerSum(a) 
 {
     return dacSumHelper(a,0,a.length - 1);
